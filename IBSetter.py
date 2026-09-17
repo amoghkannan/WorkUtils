922d7cbad4fea269e2ad4d083d84a257d93610cc
amoghkannan <95480759+amoghkannan@users.noreply.github.com>
2026-09-17 11:41:02 +0530

    Code to generate IB lines for testing

--- IBSetter.py
+++ IBSetter.py
@@ -1,3 +1,5 @@
+#Tool to generate IB lines for testing
+
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -11,6 +13,7 @@ ax.set_yticks(y)
 ax.grid()
 plt.xlim(-1.0,1.0)
 plt.ylim(-1.0,1.0)
+ax.set_aspect("equal")
 ax.tick_params(
     axis='both',       # Apply to both x and y axes
     which='both',      # Apply to major and minor ticks
@@ -38,3 +41,24 @@ def onclick(event):
 
 cid=fig.canvas.mpl_connect("button_press_event",onclick)
 plt.show()
+
+coords.append(coords[0])
+
+nIB=len(coords)
+outfile=open("files/mesh/ib.dat","w")
+
+outfile.write(str(nIB)+"\n")
+
+for i in range(0,nIB-1):
+        currCoord1=coords[i]
+        currCoord2=coords[i+1]
+        normal=np.array([currCoord2[1]-currCoord1[1],currCoord1[0]-currCoord2[0]])
+        normal=normal/np.sqrt(normal[0]**2+normal[1]**2)
+        outfile.write("%25.15f " % currCoord1[0])
+        outfile.write("%25.15f " % currCoord1[1])
+        outfile.write("%25.15f " % currCoord2[0])
+        outfile.write("%25.15f " % currCoord2[1])
+        outfile.write("%25.15f " % normal[0])
+        outfile.write("%25.15f\n" % normal[1])
+
+outfile.close()
